e6694a55d1f060bf68f47a163a358d91e250c2b5
NixowL <nixowl33t@gmail.com>
2022-07-19 21:32:40 +0100

    first commit

---
+++ src/Column.tsx
@@ -0,0 +1,69 @@
+import { ColumnContainer, ColumnTitle } from './styles'
+import { Card } from './Card'
+import { AddNewItem } from './AddNewItem'
+import { useAppState } from './state/AppStateContext'
+import { addTask, moveTask, moveList, setDraggedItem } from './state/actions'
+import { useRef } from 'react'
+import { useItemDrag } from './utils/useItemDrag'
+import { throttle } from 'throttle-debounce-ts'
+import { useDrop } from 'react-dnd'
+import { isHidden } from './utils/isHidden'
+
+type ColumnProps = {
+  text: string
+  id: string
+  isPreview?: boolean
+}
+
+export const Column = ({ text, id, isPreview }: ColumnProps) => {
+  const ref = useRef<HTMLDivElement>(null)
+  const [, drop] = useDrop({
+    accept: ['COLUMN', 'CARD'],
+    hover: throttle(200, () => {
+      if (!draggedItem) {
+        return
+      }
+      if (draggedItem.type === 'COLUMN') {
+        if (draggedItem.id === id) {
+          return
+        }
+
+        dispatch(moveList(draggedItem.id, id))
+      } else {
+        if (draggedItem.columnId === id) {
+          return
+        }
+        if (tasks.length) {
+          return
+        }
+
+        dispatch(moveTask(draggedItem.id, null, draggedItem.columnId, id))
+        dispatch(setDraggedItem({ ...draggedItem, columnId: id }))
+      }
+    }),
+  })
+  const { drag } = useItemDrag({ type: 'COLUMN', id, text })
+
+  const { draggedItem, getTasksByListId, dispatch } = useAppState()
+  const tasks = getTasksByListId(id)
+
+  drag(drop(ref))
+
+  return (
+    <ColumnContainer
+      isPreview={isPreview}
+      ref={ref}
+      isHidden={isHidden(draggedItem, 'COLUMN', id, isPreview)}
+    >
+      <ColumnTitle>{text}</ColumnTitle>
+      {tasks.map((task) => (
+        <Card columnId={id} text={task.text} key={task.id} id={task.id} />
+      ))}
+      <AddNewItem
+        toggleButtonText='+ Add another card'
+        onAdd={(text) => dispatch(addTask(text, id))}
+        dark
+      />
+    </ColumnContainer>
+  )
+}
